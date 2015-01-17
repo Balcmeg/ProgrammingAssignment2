4b3c0425d57b3874dcde83ea95c8b7fd39c9cbb4
Balcmeg <stefangutehall@Stefans-iMac-7.local>
2015-01-17 09:50:39 +0800

    Updated variablenames

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,21 @@
 ### Function makeCacheMatrix(x) take an invertible matrix as input x and cache it
 makeCacheMatrix <- function(x = matrix()) {
       ## Initialise 'm' as NULL
-      m<-NULL
+      CachedMatrix<-NULL
       ## Create local function 'set' to cache matrix. 
       ## Set take value 'y' and write to cache variable 'x' and
       ## initialise 'm' as NULL as chache matrix variable.
       set<-function(y){
             x<<-y
-            m<<-NULL
+            CachedMatrix<<-NULL
       }
       ## Create local function 'get' to fetch variable 'x' from cache
       get<-function() x
       ## Create local function 'setmatrix' to solve matrix inverse and write to 
       ## cache.
-      setmatrix<-function(solve) m<<- solve
+      setmatrix<-function(solve) CachedMatrix<<- solve
       ## Create local function 'getmatrix' to fetch cachad invere matrix .
-      getmatrix<-function() m
+      getmatrix<-function() CachedMatrix
       ## Use "list" command to create an object with the previoulsly created
       ## methods.
       list(set=set, get=get, setmatrix=setmatrix,  getmatrix=getmatrix)
